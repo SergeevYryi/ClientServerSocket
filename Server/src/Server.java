8883739d26e96ccaa18ddebc5e94ded6efa4da7e
SergeevYryi <sergeev.y90@inbox.ru>
2021-06-11 19:16:18 +0400

    part 2.1

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -3,24 +3,33 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
 
-        ServerSocket server = new ServerSocket(8000);
-        System.out.println("Server started!");
+        try (ServerSocket server = new ServerSocket(8000))
+        {
+            System.out.println("Server started!");
+            try (
+                    Socket socket = server.accept();
+                    BufferedWriter writer =
+                            new BufferedWriter(
+                                    new OutputStreamWriter(
+                                            socket.getOutputStream()));
+                    BufferedReader reader =
+                            new BufferedReader(
+                                    new InputStreamReader(
+                                            socket.getInputStream()));
+        )   {
+            String request = reader.readLine();
+            System.out.println("Request: " + request);
+            String response = "HELLO FROM SERVER: " + request.length();
+            System.out.println("Response: " + response);
+            writer.write(response);
+            writer.newLine();
+            writer.flush();
+            }
 
-        Socket socket = server.accept();
-        System.out.println("Client connected");
-
-        BufferedWriter writer =
-                new BufferedWriter(
-                        new OutputStreamWriter(
-                                socket.getOutputStream()));
-        writer.write("HELLO FROM SERVER");
-        writer.newLine();
-        writer.flush();
-        writer.close();
-        socket.close();
-        server.close();
-
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
