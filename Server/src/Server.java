cbbfcca3e2f5dcdcfcb1873b9b26ea163addda13
SergeevYryi <sergeev.y90@inbox.ru>
2021-06-11 20:00:00 +0400

    part 2.3

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -8,16 +8,17 @@ public class Server {
         try (ServerSocket server = new ServerSocket(8000))
         {
             System.out.println("Server started!");
-            try (
+            while (true)
+                try (
                     Socket socket = server.accept();
                     BufferedWriter writer =
-                            new BufferedWriter(
-                                    new OutputStreamWriter(
-                                            socket.getOutputStream()));
+                        new BufferedWriter(
+                            new OutputStreamWriter(
+                                socket.getOutputStream()));
                     BufferedReader reader =
-                            new BufferedReader(
-                                    new InputStreamReader(
-                                            socket.getInputStream()));
+                        new BufferedReader(
+                            new InputStreamReader(
+                                socket.getInputStream()));
         )   {
             String request = reader.readLine();
             System.out.println("Request: " + request);
@@ -26,6 +27,8 @@ public class Server {
             writer.write(response);
             writer.newLine();
             writer.flush();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
             }
 
         } catch (IOException e) {
