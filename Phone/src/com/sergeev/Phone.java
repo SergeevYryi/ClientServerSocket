8350f565d655ea8c372eaa2e6b88b3c16c8c3ac0
SergeevYryi <sergeev.y90@inbox.ru>
2021-06-12 19:03:03 +0400

    part 2.5

--- Phone/src/com/sergeev/Phone.java
+++ Phone/src/com/sergeev/Phone.java
@@ -8,14 +8,28 @@ public class Phone {
     private final Socket socket;
     private final BufferedReader reader;
     private final BufferedWriter writer;
+
     public Phone(Socket socket) {
         this.socket = socket;
         this.reader = createReader ();
         this.writer = createWriter ();
     }
-    public Phone (ServerSocket server) {
-        this.socket = server.accept();
-    }
+        public void writeLine (String message) {
+        try {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        }
+        public String readLine() {
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     private BufferedReader createReader() {
         try {
             return new BufferedReader(new InputStreamReader(socket.getInputStream()));
